6f59cf5ca527239af2437765513281888a6c66bf
ri-peter <peter.whyte@river-island.com>
2024-03-20 16:32:47 +0000

    testing the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ri-peter.github.io',
-  base: '/Astro-blog',
+  site: 'https://ri-peter.github.io/Astro-blog',
   integrations: [preact()]
 });
