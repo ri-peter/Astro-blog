f11efd631fb30b81e9603c4e0436b5f914dc3bbf
ri-peter <peter.whyte@river-island.com>
2024-03-21 09:34:36 +0000

    Fixing base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+const isProd = import.meta.env.PROD;
+const baseUrl = isProd ? "/Astro-blog" : "";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ri-peter.github.io/Astro-blog',
+  site: 'https://ri-peter.github.io/',
+  base: baseUrl,
   integrations: [preact()]
 });
